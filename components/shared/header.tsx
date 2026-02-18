c234fb3b36bddc85ea5469c0150d3ee7203f96af
mbpxl <esscof1e@gmail.com>
2026-02-18 23:27:42 +0300

    first commit

---
+++ components/shared/header.tsx
@@ -0,0 +1,41 @@
+import { ShoppingCart, User, ArrowRight } from "lucide-react";
+import { Button } from "../ui";
+import { Container } from "./container";
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header className="border border-b">
+      <Container className="flex items-center justify-between py-8">
+        <div className="flex gap-2">
+          <Image src="/logo.png" alt="logo" width={35} height={45} />
+          <div className="">
+            <h1 className="text-2xl uppercase font-black">Pizza Room</h1>
+            <p className="text-sm text-gray-400 leading-3">лучше не бывает</p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button variant={"outline"} className="flex items-center gap-1">
+            <User size={16} /> Войти
+          </Button>
+
+          <div className="group relative">
+            <Button>
+              <b>520 ₽</b>
+              <span className="h-full w-px bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart size={16} className="relative" strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight
+                size={20}
+                className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+              />
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
