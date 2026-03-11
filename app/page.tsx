35fc6e8f4aacde1e757656593e8a661ec6a48658
mbpxl <esscof1e@gmail.com>
2026-03-11 23:18:44 +0300

    feeature(main page): add checkbox filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from "@/components/shared";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -7,6 +7,23 @@ export default function Home() {
         <Title text={"Все пиццы"} size="lg" className="font-extrabold" />
       </Container>
       <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-15">
+          {/* фильтрация */}
+          <div className="w-62.5">
+            <Filters />
+          </div>
+
+          {/* товары */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {/* <ProductGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+              <ProductGroupList title="Комбо" items={[1, 2, 3, 4, 5]} /> */}
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
